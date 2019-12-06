f8f3b7a0711f009ebb09a5c3882a6f86fa5c0871
Simon Bekemeier <simon.bekemeier@fh-bielefeld.de>
2019-12-06 14:42:03 +0100

    Combine set and check of geb_jahr
    'Force' sanity check for date of birth (geb_jahr) when it is set.

--- source.cpp
+++ source.cpp
@@ -21,10 +21,15 @@ struct student
     int geb_jahr;
 };
 
+void set_mitarbeiter_geb(struct mitarbeiter * mitarbeiter, int input);
+void set_student_geb(struct student * student, int input);
+
 int main(int argc, char ** argv)
 {
     struct student student;
     struct mitarbeiter mitarbeiter;
+
+    int input;
 
     printf("Name: ");
     scanf("%s", student.name);
@@ -35,12 +40,8 @@ int main(int argc, char ** argv)
     printf("Studiengang: ");
     scanf("%s", &student.sgang);
     printf("Geb. Jahr: ");
-    scanf("%d", &student.geb_jahr);
-    if (student.geb_jahr > student.imatjahr) {
-        printf("Das macht keinen Sinn!\n");
-        system("pause");
-        return -1;
-    }
+    scanf("%d", &input);
+    set_student_geb(&student, input);
 
     printf("\n\n");
 
@@ -57,12 +58,8 @@ int main(int argc, char ** argv)
     printf("Einst. Jahr: ");
     scanf("%d", &mitarbeiter.einst_jahr);
     printf("Geb. Jahr: ");
-    scanf("%d", &mitarbeiter.geb_jahr);
-    if (mitarbeiter.geb_jahr > mitarbeiter.einst_jahr) {
-        printf("Das macht keinen Sinn!\n");
-        system("pause");
-        return -1;
-    }
+    scanf("%d", &input);
+    set_mitarbeiter_geb(&mitarbeiter, input);
 
     printf("\n\n");
 
@@ -73,4 +70,24 @@ int main(int argc, char ** argv)
     system("pause");
 
     return 0;
+}
+
+void set_student_geb(struct student * student, int input)
+{
+    student->geb_jahr = input;
+    if (student->geb_jahr > student->imatjahr) {
+        printf("Das macht keinen Sinn!\n");
+        system("pause");
+        exit(-1);
+    }
+}
+
+void set_mitarbeiter_geb(struct mitarbeiter * mitarbeiter, int input)
+{
+    mitarbeiter->geb_jahr = input;
+    if (mitarbeiter->geb_jahr > mitarbeiter->einst_jahr) {
+        printf("Das macht keinen Sinn!\n");
+        system("pause");
+        exit(-1);
+    }
 }
